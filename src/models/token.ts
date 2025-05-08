c9081cd52477c21ee5ea860ec74408184c029ef8
LUCHER4321 <lucher4321@gmail.com>
2025-05-08 19:37:00 -0400

    Update token.ts

--- src/models/token.ts
+++ src/models/token.ts
@@ -1,9 +1,9 @@
 import { db } from "./connection";
 
 export const validateToken = async (token: string, user: string | undefined = undefined): Promise<boolean> => {
-    const addUser = user ? "AND hex(id) = ?" : "";
+    const addUser = user ? "AND HEX(id) = ?" : "";
     const [validation] = (await db.execute({
-        sql: `SELECT token FROM token, user WHERE user_id = id AND hex(token) = ? ${addUser} GROUP BY token;`,
+        sql: `SELECT token FROM token, user WHERE user_id = id AND HEX(token) = ? ${addUser} GROUP BY token;`,
         args: user ? [token, user] : [token],
     })).rows;
     if(!validation) return false;
