a32839f7a9e39e4e6949d5f90e2ef01e6f24d731
LUCHER4321 <lucher4321@gmail.com>
2025-05-08 19:37:24 -0400

    Update route.ts

--- src/routes/route.ts
+++ src/routes/route.ts
@@ -1,14 +1,19 @@
 import { Router } from "express";
 import { Controller } from "../types"
 
-export const createRouter = ({ controller }: { controller: Controller }) => {
+interface routerProps {
+    controller: Controller;
+    previous?: string;
+}
+
+export const createRouter = ({ controller, previous }: routerProps) => {
     const router = Router();
-    router.get("/", controller.getAll);
-    router.get("/:id", controller.getById);
-    router.post("/", controller.create);
-    router.patch("/:id", controller.update);
-    router.delete("/:id", controller.delete);
-    router.options("/", (_, res) => {
+    router.get(previous + "/", controller.getAll);
+    router.get(previous + "/:id", controller.getById);
+    router.post(previous + "/", controller.create);
+    router.patch(previous + "/:id", controller.update);
+    router.delete(previous + "/:id", controller.delete);
+    router.options(previous + "/", (_, res) => {
         res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
         res.send(200);
     });
