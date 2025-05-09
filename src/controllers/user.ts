d140d9d732dc6ee6a47451b24e7e58d3065847b8
LUCHER4321 <lucher4321@gmail.com>
2025-05-09 14:54:24 -0400

    Update user.ts

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,10 +1,9 @@
 import { Request, Response } from "express";
-import { getUser } from "../models/user";
 
-export const getUserController = async (req: Request, res: Response) => {
+export const getUserController = (getUser: (t: string) => Promise<any>) => async (req: Request, res: Response) => {
     const { token } = req.params;
     try {
-        const user = await getUser({ token });
+        const user = await getUser(token);
         res.json(user);
     } catch(e: any) {
         res.status(404).json({ message: e.message });
