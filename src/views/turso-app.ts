676142d6bcd7684b0dea63b37da0b5f6e7d3f572
LUCHER4321 <lucher4321@gmail.com>
2025-05-09 14:54:42 -0400

    Update turso-app.ts

--- src/views/turso-app.ts
+++ src/views/turso-app.ts
@@ -1,9 +1,13 @@
 import { categoryController } from "../controllers/categories";
 import { languageController } from "../controllers/languages";
 import { projectController } from "../controllers/projects";
+import { getTranslationsController } from "../controllers/translations";
+import { getUserController } from "../controllers/user";
 import { categoryModel } from "../models/categories";
 import { languageModel } from "../models/languages";
 import { projectModel } from "../models/projects";
+import { getTranslations } from "../models/translations";
+import { getUser } from "../models/user";
 import { createAPI } from "../routes/api-portfolio";
 import { createApp } from "./app";
 
@@ -12,5 +16,7 @@ export const app = createApp({
         languageController: languageController(languageModel),
         categoryController: categoryController(categoryModel),
         projectController: projectController(projectModel),
+        getTranslationsController: getTranslationsController(getTranslations),
+        getUserController: getUserController(getUser),
     }),
 });
