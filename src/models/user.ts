c17ddaaf46744a6f26223d2d226b29ef031da53c
LUCHER4321 <lucher4321@gmail.com>
2025-05-09 14:54:20 -0400

    Update user.ts

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,6 +1,6 @@
 import { db } from "./connection";
 
-export const getUser = async ({ token }: { token: string }) => {
+export const getUser = async (token: string) => {
     try {
         const [result] = (await db.execute({
             sql: "SELECT HEX(id) as id, name FROM user, token WHERE user_id = id and HEX(token) = ? GROUP BY token;",
