0bcf9b9e49e0ef895165719a5045a8b251fc6999
LUCHER4321 <lucher4321@gmail.com>
2025-05-09 22:36:58 -0400

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -3,3 +3,4 @@ import 'dotenv/config';
 export const PORT = process.env.PORT ? parseInt(process.env.PORT) : 3000;
 export const URL = process.env.DB_URL ?? "";
 export const TOKEN = process.env.DB_TOKEN ?? "";
+export const ACCEPTED_ORIGINS = process.env.ACCEPTED_ORIGINS ? process.env.ACCEPTED_ORIGINS.split("||") : [];
