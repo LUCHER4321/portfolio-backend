68f9f8aad42f5a836e4c71870635910dae45a401
LUCHER4321 <lucher4321@gmail.com>
2025-05-11 14:27:25 -0400

    2nd Deploy

--- src/config.ts
+++ src/config.ts
@@ -4,3 +4,4 @@ export const PORT = process.env.PORT ? parseInt(process.env.PORT) : 3000;
 export const URL = process.env.DB_URL ?? "";
 export const TOKEN = process.env.DB_TOKEN ?? "";
 export const ACCEPTED_ORIGINS = process.env.ACCEPTED_ORIGINS ? process.env.ACCEPTED_ORIGINS.split("||") : [];
+export const VERCEL = process.env.VERCEL ?? "";
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
-import { PORT } from "./config";
+import { VercelRequest, VercelResponse } from "@vercel/node";
+import { PORT, VERCEL } from "./config";
 import { app } from "./views/turso-app";
 
-module.exports = app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+export default (req: VercelRequest, res: VercelResponse) => app(req, res);
+
+if(VERCEL !== "1") {
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+}
