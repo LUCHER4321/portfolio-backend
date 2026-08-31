e36c529ddafc7d714c898994d20c65f91f61f89f
LUCHER4321 <lucher4321@gmail.com>
2026-08-30 21:59:48 -0400

    Create contact.ts

---
+++ src/models/contact.ts
@@ -0,0 +1,19 @@
+import { MAIL, MAILER_USER } from "../config";
+import { ContactModel } from "../types";
+import { transporter } from "./connection";
+import { validateToken } from "./token";
+
+export const contactModel: ContactModel = {
+    contact: async ({ token, name, company, email, phone, message }) => {
+        const validation = await validateToken(token);
+        if (!validation) throw new Error("Invalid token");
+        await transporter.sendMail({
+            from: `"No Reply" <${MAILER_USER}>`,
+            to: MAIL,
+            subject: `Contact mail from ${name} (${email})`,
+            text: `Company: ${company}
+            Phone: ${phone}
+            ${message}`
+        });
+    }
+}
