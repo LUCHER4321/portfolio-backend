822aecbc4c91ffc33d7ef2aa6bb19c8276023b37
LUCHER4321 <lucher4321@gmail.com>
2026-08-30 21:59:50 -0400

    Update connection.ts

--- src/models/connection.ts
+++ src/models/connection.ts
@@ -1,7 +1,26 @@
 import { createClient } from "@libsql/client";
-import { TOKEN, URL } from "../config";
+import { MAILER_PASS, MAILER_USER, TOKEN, URL } from "../config";
+import { createTransport } from "nodemailer";
 
 export const db = createClient({
     url: URL,
     authToken: TOKEN,
 });
+
+export const transporter = createTransport({
+    service: "Gmail",
+    auth: {
+        user: MAILER_USER,
+        pass: MAILER_PASS
+    },
+    tls: {
+        rejectUnauthorized: false
+    }
+});
+
+transporter.verify()
+    .then(() => console.log("Ready for send emails"))
+    .catch(e => {
+        console.error("Error configuring email:", (e as Error).message);
+        console.error("Details:", e);
+    });
